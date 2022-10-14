f5b529f472c06b5748b1b4f1a461377877bfa1eb
david@akopyan.lv <david@akopyan.lv>
2022-10-14 15:01:33 +0300

    - versioning init fix

--- src/DependencyInjection/RestExtension.php
+++ src/DependencyInjection/RestExtension.php
@@ -24,6 +24,8 @@ class RestExtension extends Extension implements PrependExtensionInterface
         $config = $this->processConfiguration(new Configuration(), $configs);
 
         $this->loadBodyConverter($config, $loader, $container);
+        $this->loadQueryConverter($config, $loader, $container);
+        $this->loadVersioning($config, $loader, $container);
     }
 
     /**
@@ -67,6 +69,40 @@ class RestExtension extends Extension implements PrependExtensionInterface
         if (!empty($config['query_converter']['validation_errors_argument'])) {
             $container->getDefinition('dmp_rest.converter.request_query')
                 ->replaceArgument('$validationErrorsArgument', $config['query_converter']['validation_errors_argument']);
+        }
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function loadVersioning(array $config, YamlFileLoader $loader, ContainerBuilder $container): void
+    {
+        if (!empty($config['versioning']['enabled'])) {
+            $loader->load('versioning.yaml');
+
+            $versionListener = $container->getDefinition('dmp_rest.versioning.listener');
+            $versionListener->replaceArgument(1, $config['versioning']['default_version']);
+
+            $resolvers = [];
+            if ($config['versioning']['resolvers']['query']['enabled']) {
+                $resolvers['query'] = $container->getDefinition('dmp_rest.versioning.query_parameter_resolver');
+                $resolvers['query']->replaceArgument(0, $config['versioning']['resolvers']['query']['parameter_name']);
+            }
+            if ($config['versioning']['resolvers']['custom_header']['enabled']) {
+                $resolvers['custom_header'] = $container->getDefinition('dmp_rest.versioning.header_resolver');
+                $resolvers['custom_header']->replaceArgument(0, $config['versioning']['resolvers']['custom_header']['header_name']);
+            }
+            if ($config['versioning']['resolvers']['media_type']['enabled']) {
+                $resolvers['media_type'] = $container->getDefinition('dmp_rest.versioning.media_type_resolver');
+                $resolvers['media_type']->replaceArgument(0, $config['versioning']['resolvers']['media_type']['regex']);
+            }
+
+            $chainResolver = $container->getDefinition('dmp_rest.versioning.chain_resolver');
+            foreach ($config['versioning']['guessing_order'] as $resolver) {
+                if (isset($resolvers[$resolver])) {
+                    $chainResolver->addMethodCall('addResolver', [$resolvers[$resolver]]);
+                }
+            }
         }
     }
 
